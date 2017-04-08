2ae56e988f5f173a1633ab640932ae1ee26cf2f0
Jacob Fischer <jacob.t.fischer@gmail.com>
2017-04-07 22:26:45 -0500

    Add tostring and concat methods to all game objects

--- joueur/baseGameObject.lua
+++ joueur/baseGameObject.lua
@@ -11,4 +11,12 @@ function BaseGameObject:_runOnServer(functionName, args)
     return require("joueur.client"):runOnServer(self, functionName, args)
 end
 
+function BaseGameObject:__tostring()
+    return self.gameObjectName .. " #" .. self.id
+end
+
+function BaseGameObject:__concat(right)
+    return tostring(self) .. tostring(right)
+end
+
 return BaseGameObject
