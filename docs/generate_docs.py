0fb34937bf84690fee9cd7cc7e3db6af257d0657
Jacob Fischer <jacob.t.fischer@gmail.com>
2018-03-16 21:57:27 -0500

    Add travis deploying docs

--- docs/generate_docs.py
+++ docs/generate_docs.py
@@ -1,8 +1,8 @@
 import os
 import os.path
+import datetime
 import shutil
 import subprocess
-import argparse
 import re
 import markdown # pip module
 import sys
@@ -12,31 +12,49 @@ def run(*args, **kwargs):
     if error_code != 0: # an error happened
         sys.exit(error_code)
 
-parser = argparse.ArgumentParser(description='Runs the python 3 client doc generation script.')
-parser.add_argument('game', action='store', help='the name of the game you want to document. Must exist in ../games/')
-
-args = parser.parse_args()
-
-game_name = args.game[0].upper() + args.game[1:]
-lower_game_name = game_name[0].lower() + game_name[1:]
-
 if os.path.isdir("./output"):
     shutil.rmtree("./output")
+os.makedirs("./output")
+
+shutil.copyfile("./ldoc.css", "./output/ldoc.css")
+
+game_names = next(os.walk('../games'))[1]
+
+with open("./index.html", "r") as index_file:
+   index_contents = index_file.read()
 
 with open("../README.md", "r") as md_readme:
     readme = md_readme.read()
 
-readme = readme.replace("GAME_NAME", game_name).replace("game_name", lower_game_name)
-
 html_readme = markdown.markdown(readme)
 
-with open("./config.ld", "w+") as config:
-    config.write('full_description=[[{}]]'.format(html_readme))
+a = index_contents
+a = a.replace("___GAMES___", '  \n'.join('<li><a href="{}/">{}</a></li>'.format(n, n[0].upper() + n[1:]) for n in game_names))
+a = a.replace("___README___", html_readme)
+a = a.replace("___YEAR___", str(datetime.datetime.now().year))
+new_index_contents = a
 
-run(['ldoc --dir ./output --title "{game_name} Lua Client" --project "{game_name} Lua Client" --config ./config.ld --verbose ../games/{lower_game_name}'.format(
-    game_name=game_name,
-    lower_game_name=lower_game_name,
-)], shell=True)
+with open("./output/index.html", "w+") as new_index:
+    new_index.write(new_index_contents)
 
-# cleanup files we made
-os.remove("config.ld")
+for lower_game_name in game_names:
+    game_name = lower_game_name[0].upper() + lower_game_name[1:]
+
+    with open("./config.ld", "w+") as config:
+        config.write("""
+project = [[<p style="margin: 0.25em;"><a href="../">Lua Joueur Client</a></p>
+<p style="font-size: 0.675em; margin: 0.5em;">{game_name} Game</p>
+]]
+title = '{game_name} Lua Client'
+-- full_descriptions = [[{html_readme}]]
+""".format(
+    html_readme='hi' or html_readme,
+    game_name=game_name
+))
+
+    run(['ldoc --dir ./output/{lower_game_name} --config ./config.ld --verbose ../games/{lower_game_name}'.format(
+        lower_game_name=lower_game_name
+    )], shell=True)
+
+    # cleanup files we made
+    os.remove("config.ld")
