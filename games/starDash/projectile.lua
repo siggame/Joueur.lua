f419fe25fc2e65ccbdb33cf89b56b786d5cfa4cd
Strahm, Jeffrey S.(S&T-Student) <jssfg8@mst.edu>
2019-02-13 17:34:19 -0600

    Updated stardash's codegen

--- games/starDash/projectile.lua
+++ games/starDash/projectile.lua
@@ -1,4 +1,4 @@
--- Job: Information about a unit's job.
+-- Projectile: Tracks any projectiles moving through space.
 -- DO NOT MODIFY THIS FILE
 -- Never try to directly create an instance of this class, or modify its member variables.
 -- Instead, you should only be reading its variables and calling its functions.
@@ -11,30 +11,26 @@ local GameObject = require("games.stardash.gameObject")
 -- you can add additional require(s) here
 -- <<-- /Creer-Merge: requires -->>
 
---- Information about a unit's job.
--- @classmod Job
-local Job = class(GameObject)
+--- Tracks any projectiles moving through space.
+-- @classmod Projectile
+local Projectile = class(GameObject)
 
--- initializes a Job with basic logic as provided by the Creer code generator
-function Job:init(...)
+-- initializes a Projectile with basic logic as provided by the Creer code generator
+function Projectile:init(...)
     GameObject.init(self, ...)
 
     -- The following values should get overridden when delta states are merged, but we set them here as a reference for you to see what variables this class has.
 
-    --- How many combined resources a unit with this Job can hold at once.
-    self.carryLimit = 0
-    --- The amount of damage this Job does per attack.
-    self.damage = 0
-    --- The amount of starting health this Job has.
-    self.energy = 0
-    --- The distance this job can move per turn.
-    self.moves = 0
-    --- The reserve the martyr use to protect allies.
-    self.shield = 0
-    --- The Job title. 'corvette', 'missleboat', 'martyr', 'transport', or 'miner'. (in this order from 0-4).
-    self.title = ""
-    --- How much money it costs to spawn a unit.
-    self.unitCost = 0
+    --- The Player that owns and can control this Unit.
+    self.owner = nil
+    --- The radius of the circle this projectile occupies.
+    self.radius = 0
+    --- The unit that is being attacked by this projectile.
+    self.target = nil
+    --- The x value this projectile is on.
+    self.x = 0
+    --- The y value this projectile is on.
+    self.y = 0
 
     --- (inherited) String representing the top level Class that this game object is an instance of. Used for reflection to create new instances on clients, but exposed for convenience should AIs want this data.
     -- @field[string] self.gameObjectName
@@ -52,7 +48,7 @@ function Job:init(...)
 end
 
 --- (inherited) Adds a message to this GameObject's logs. Intended for your own debugging purposes, as strings stored here are saved in the gamelog.
--- @function Job:log
+-- @function Projectile:log
 -- @see GameObject:log
 -- @tparam string message A string to add to this GameObject's log. Intended for debugging.
 
@@ -61,4 +57,4 @@ end
 -- if you want to add any client side logic this is where you can add them
 -- <<-- /Creer-Merge: functions -->>
 
-return Job
+return Projectile
