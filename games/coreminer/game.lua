f5887045405086bc75a2a41814f9a5c3eada28b0
Jonathan Henderson <jkhenderson999@gmail.com>
2020-02-12 23:03:27 -0600

    Add Coreminer

---
+++ games/coreminer/game.lua
@@ -0,0 +1,88 @@
+-- Game: Mine resources to obtain more wealth than your opponent.
+-- DO NOT MODIFY THIS FILE
+-- Never try to directly create an instance of this class, or modify its member variables.
+-- Instead, you should only be reading its variables and calling its functions.
+
+
+local class = require("joueur.utilities.class")
+local BaseGame = require("joueur.baseGame")
+
+-- <<-- Creer-Merge: requires -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
+-- you can add additional require(s) here
+-- <<-- /Creer-Merge: requires -->>
+
+--- Mine resources to obtain more wealth than your opponent.
+-- @classmod Game
+local Game = class(BaseGame)
+
+-- initializes a Game with basic logic as provided by the Creer code generator
+function Game:init(...)
+    BaseGame.init(self, ...)
+
+    --- The name of this game, "{game_name}".
+    -- @field[string] self.name
+    -- The following values should get overridden when delta states are merged, but we set them here as a reference for you to see what variables this class has.
+
+    --- The player whose turn it is currently. That player can send commands. Other players cannot.
+    self.currentPlayer = nil
+    --- The current turn number, starting at 0 for the first player's turn.
+    self.currentTurn = 0
+    --- The amount of turns it takes to gain a free Bomb.
+    self.freeBombInterval = 0
+    --- A mapping of every game object's ID to the actual game object. Primarily used by the server and client to easily refer to the game objects via ID.
+    self.gameObjects = Table()
+    --- A array-like table of all jobs.
+    self.jobs = Table()
+    --- The number of Tiles in the map along the y (vertical) axis.
+    self.mapHeight = 0
+    --- The number of Tiles in the map along the x (horizontal) axis.
+    self.mapWidth = 0
+    --- The maximum number of turns before the game will automatically end.
+    self.maxTurns = 100
+    --- The amount of victory points awarded when ore is deposited in the base.
+    self.oreValue = 0
+    --- List of all the players in the game.
+    self.players = Table()
+    --- A unique identifier for the game instance that is being played.
+    self.session = ""
+    --- All the tiles in the map, stored in Row-major order. Use `x + y * mapWidth` to access the correct index.
+    self.tiles = Table()
+    --- The amount of time (in nano-seconds) added after each player performs a turn.
+    self.timeAddedPerTurn = 0
+    --- Every Unit in the game.
+    self.units = Table()
+    --- The amount of victory points required to win.
+    self.victoryAmount = 0
+
+
+
+    self.name = "Coreminer"
+
+    self._gameVersion = "ebf8479aecc1663fdbdbd350b23ed190fc0b97b892200e752100f5509cb0d1a2"
+    self._gameObjectClasses = {
+        GameObject = require("games.coreminer.gameObject"),
+        Job = require("games.coreminer.job"),
+        Player = require("games.coreminer.player"),
+        Tile = require("games.coreminer.tile"),
+        Unit = require("games.coreminer.unit"),
+    }
+end
+
+
+--- Gets the Tile at a specified (x, y) position
+-- @tparam number x integer between 0 and the mapWidth
+-- @tparam number y integer between 0 and the mapHeight
+-- @treturns Tile the Tile at (x, y) or nil if out of bounds
+function Game:getTileAt(x, y)
+    if x < 0 or y < 0 or x >= self.mapWidth or y >= self.mapHeight then -- out of bounds
+        return nil;
+    end
+
+    return self.tiles[1 + x + y * self.mapWidth] -- 1 + ... because Lua lists are indexed at 1, so we add 1 to indexes, but we don't for the x, y positions
+end
+
+-- <<-- Creer-Merge: functions -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
+-- if you want to add any client side logic this is where you can add them
+-- <<-- /Creer-Merge: functions -->>
+
+return Game
---
+++ games/coreminer/unit.lua
@@ -0,0 +1,124 @@
+-- Unit: A unit in the game.
+-- DO NOT MODIFY THIS FILE
+-- Never try to directly create an instance of this class, or modify its member variables.
+-- Instead, you should only be reading its variables and calling its functions.
+
+
+local class = require("joueur.utilities.class")
+local GameObject = require("games.coreminer.gameObject")
+
+-- <<-- Creer-Merge: requires -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
+-- you can add additional require(s) here
+-- <<-- /Creer-Merge: requires -->>
+
+--- A unit in the game.
+-- @classmod Unit
+local Unit = class(GameObject)
+
+-- initializes a Unit with basic logic as provided by the Creer code generator
+function Unit:init(...)
+    GameObject.init(self, ...)
+
+    -- The following values should get overridden when delta states are merged, but we set them here as a reference for you to see what variables this class has.
+
+    --- The number of bombs being carried by this Unit. (0 to job cargo capacity - other carried materials).
+    self.bombs = 0
+    --- The number of building materials carried by this Unit. (0 to job cargo capacity - other carried materials).
+    self.buildingMaterials = 0
+    --- The amount of dirt carried by this Unit. (0 to job cargo capacity - other carried materials).
+    self.dirt = 0
+    --- The remaining health of a Unit.
+    self.health = 0
+    --- The Job this Unit has.
+    self.job = nil
+    --- The remaining mining power this Unit has this turn.
+    self.miningPower = 0
+    --- The number of moves this Unit has left this turn.
+    self.moves = 0
+    --- The amount of ore carried by this Unit. (0 to job capacity - other carried materials).
+    self.ore = 0
+    --- The Player that owns and can control this Unit.
+    self.owner = nil
+    --- The Tile this Unit is on.
+    self.tile = nil
+
+    --- (inherited) String representing the top level Class that this game object is an instance of. Used for reflection to create new instances on clients, but exposed for convenience should AIs want this data.
+    -- @field[string] self.gameObjectName
+    -- @see GameObject.gameObjectName
+
+    --- (inherited) A unique id for each instance of a GameObject or a sub class. Used for client and server communication. Should never change value after being set.
+    -- @field[string] self.id
+    -- @see GameObject.id
+
+    --- (inherited) Any strings logged will be stored here. Intended for debugging.
+    -- @field[{string, ...}] self.logs
+    -- @see GameObject.logs
+
+
+end
+
+--- Builds a support, shield, ladder, or bomb on Unit's tile, or an adjacent Tile.
+-- @tparam Tile tile The Tile to build on.
+-- @tparam string type The structure to build (support, ladder, shield, or bomb).
+-- @treturn bool True if successfully built, False otherwise.
+function Unit:build(tile, type)
+    return not not (self:_runOnServer("build", {
+        tile = tile,
+        type = type,
+    }))
+end
+
+--- Dumps materials from cargo to an adjacent tile.
+-- @tparam Tile tile The tile the materials will be dumped on.
+-- @tparam string material The material the Unit will drop. 'dirt' or 'ore'.
+-- @tparam number amount The number of materials to drop. Amounts <= 0 will drop all the materials.
+-- @treturn bool True if successfully dumped materials, false otherwise.
+function Unit:dump(tile, material, amount)
+    return not not (self:_runOnServer("dump", {
+        tile = tile,
+        material = material,
+        amount = amount,
+    }))
+end
+
+--- Mines the Tile the Unit is on or an adjacent tile.
+-- @tparam Tile tile The Tile the materials will be mined from.
+-- @tparam number amount The amount of material to mine up. Amounts <= 0 will mine all the materials that the Unit can.
+-- @treturn bool True if successfully mined, false otherwise.
+function Unit:mine(tile, amount)
+    return not not (self:_runOnServer("mine", {
+        tile = tile,
+        amount = amount,
+    }))
+end
+
+--- Moves this Unit from its current Tile to an adjacent Tile.
+-- @tparam Tile tile The Tile this Unit should move to.
+-- @treturn bool True if it moved, false otherwise.
+function Unit:move(tile)
+    return not not (self:_runOnServer("move", {
+        tile = tile,
+    }))
+end
+
+--- Upgrade an attribute of this Unit. "health", "miningPower", "moves", or "capacity".
+-- @tparam string attribute The attribute of the Unit to be upgraded.
+-- @treturn bool True if successfully upgraded, False otherwise.
+function Unit:upgrade(attribute)
+    return not not (self:_runOnServer("upgrade", {
+        attribute = attribute,
+    }))
+end
+
+--- (inherited) Adds a message to this GameObject's logs. Intended for your own debugging purposes, as strings stored here are saved in the gamelog.
+-- @function Unit:log
+-- @see GameObject:log
+-- @tparam string message A string to add to this GameObject's log. Intended for debugging.
+
+
+
+-- <<-- Creer-Merge: functions -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
+-- if you want to add any client side logic this is where you can add them
+-- <<-- /Creer-Merge: functions -->>
+
+return Unit
